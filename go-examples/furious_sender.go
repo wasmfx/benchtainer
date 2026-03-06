f6ea7f834c011d9c5261d2c39db17498ec802859
Ezra e. k. Cooper <ezra.cooper@ed.ac.uk>
2026-03-06 12:37:30 +0000

    Use n+1 as the "infinite" channel limit: more robust when n changes.

--- go-examples/furious_sender.go
+++ go-examples/furious_sender.go
@@ -17,7 +17,7 @@ func runsum(n int) {
   // The second parameter is a channel depth. Sending blocks the thread exactly when that depth is
   // full, and no sooner. If it blocks it will trigger a deadlock detector that recognizes there is
   // no consumer. So to test raw sends, we use a large channel depth.
-  c := make(chan int, 100000000)
+  c := make(chan int, n+1)
   // You can also test send+receive together using this consumer; in that case set the depth to n-1.
   // go consumer(c)
   start := time.Now()
