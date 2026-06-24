e34d1ef1a699cbdb300aa880cc551c6301e5528f
yinamy <yinamy@outlook.com>
2026-06-24 13:28:24 +0100

    Facator out state handler

--- ocaml-examples/bin/itersum.ml
+++ ocaml-examples/bin/itersum.ml
@@ -1,4 +1,3 @@
-(* Source: https://github.com/matthew-mojira/js_of_ocaml/blob/native-effects/wasm/programs/gen2.ml *)
 open Effect
 open Effect.Deep
 
@@ -9,41 +8,16 @@ let count_up n =
     perform (Yield i)
   done
 
-(* Tracks the generator's lifecycle *)
-type gen_state =
-  | Ready
-  | Suspended of (unit, int option) continuation
-  | Done
-
-let make_generator f arg =
-  let state = ref Ready in
-  let handler = {
-    retc = (fun () -> state := Done; None);
-    exnc = raise;
-    effc = fun (type a) (eff : a Effect.t) ->
-      match eff with
-      | Yield n -> Some (fun (k : (a, int option) continuation) ->
-          state := Suspended k;
-          Some n)
-      | _ -> None
-  } in
-  let next () =
-    match !state with
-    | Done        -> None
-    | Ready       -> match_with f arg handler
-    | Suspended k -> continue k ()
-  in
-  next
-
 let () =
   let result = ref 0 in
-  let next = make_generator count_up 100000000 in
-  let rec loop () =
-    match next () with
-    | None   -> ()
-    | Some n ->
-        result := !result + n;
-        loop ()
-  in
-  loop ();
-  Printf.printf "The sum is %d\n" !result
+    match_with count_up 100000000
+    { retc = (fun () -> ());
+      exnc = raise;
+      effc = fun (type a) (eff : a Effect.t) ->
+        match eff with
+          | Yield n -> Some (fun (k : (a, _) continuation) ->
+              result := !result + n;
+              continue k ())
+          | _ -> None };
+    (* Todo: replace this with some output validation *)
+    Printf.printf "Sum: %d\n" !result
